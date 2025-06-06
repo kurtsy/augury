1decb667404051ac11ca6c2884dfde466a67d8d8
Kurt Yalcin <kyalcin@gmail.com>
2025-06-06 15:30:07 -0700

    Update script.js to use live Render URL

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ async function fetchBirdSymbolism(species) {
   debugger;
 
   try {
-    const response = await fetch("http://localhost:3000/api/omen", {
+    const response = await fetch("https://augury.onrender.com/api/omen", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ species })
